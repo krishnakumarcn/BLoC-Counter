380dc1e59f7d397f847b435bba3ece437e6655cc
krishnakumarcn <krishnacnskp@gmail.com>
2021-01-11 17:58:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:bloc_counter/counter_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'app.dart';
+
+void main() {
+  Bloc.observer = CounterObserver();
+  runApp(const CounterApp());
+}
